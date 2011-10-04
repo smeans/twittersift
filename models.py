51cec252bdd6dbf730d3f2c3f74baaca153c36a9
smeans <smeans@gmail.com>
2011-10-03 23:35:48 -0400

    initial checkin

---
+++ models.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import logging
+import datetime
+
+from google.appengine.ext import blobstore
+from google.appengine.ext import db
+from google.appengine.api import users
+
+class MyModel(db.Model):
+  pass
+
